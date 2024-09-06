0c8c629c33bcf57e4597a91a79f24e53d83dc16f
Casey <arramine@mail.ru>
2024-09-06 15:23:40 +0300

    add grid

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,124 @@ const changeTexts = () => {
         addDesktopText();
     }
 }
-changeTexts()
+changeTexts();
 
-window.addEventListener('resize', () => changeTexts())
+window.addEventListener('resize', () => {
+  changeTexts();
+  checkSliderGrid();
+  updateButtons();
+})
+
+// сетка
+
+const setSlidePrev = (currentSlide) => {
+  currentSlide = (currentSlideGrid > 0) ? currentSlide - 1 : slides.length - 1;
+  return currentSlide;
+}
+
+const setSlideNext = (currentSlide) => {
+  currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
+  return currentSlide;
+}
+
+const buttonPrevGrid = document.querySelector('#buttonPrevGrid');
+const buttonNextGrid = document.querySelector('#buttonNextGrid');
+
+const blocksForGrid = document.querySelectorAll('.grid__slide');
+const slides = [
+  [0, 1],  // Блоки 1 и 2 (для первого слайда)
+  [2],     // Блок 3 (для второго слайда)
+  [3, 4],  // Блоки 4 и 5 (для третьего слайда)
+  [5],     // Блок 6 (для четвертого слайда)
+  [6]      // Блок 7 (для пятого слайда)
+];
+let currentSlideGrid = 0;
+
+function mergeBlocksContent(blockIndexes) {
+  const slideContainer = blocksForGrid[blockIndexes[0]].cloneNode(true);
+  if (blockIndexes.length > 1) {
+    const secondBlockContent = blocksForGrid[blockIndexes[1]].querySelector('.grid__item').cloneNode(true);
+    slideContainer.appendChild(secondBlockContent);
+  }
+  return slideContainer;
+}
+
+function showSlideGrid(index) {
+  const slider = document.querySelector('.grid__slider');
+  slider.innerHTML = '';
+  const slideContent = mergeBlocksContent(slides[index]);
+  slider.appendChild(slideContent);
+}
+
+const paginationDots = document.querySelectorAll('.grid__pagination-dot');
+
+function updatePagination() {
+  paginationDots.forEach((dot, index) => {
+    dot.classList.remove('grid__pagination-dot_active');
+    if (index === currentSlideGrid) {
+      dot.classList.add('grid__pagination-dot_active');
+    }
+  });
+}
+
+const updateButtons = () => {
+  if (currentSlideGrid === 0) {
+    buttonPrevGrid.classList.add('grid__slider-button_disabled');
+  } else {
+    buttonPrevGrid.classList.remove('grid__slider-button_disabled');
+  }
+
+  if (currentSlideGrid === slides.length - 1) {
+    buttonNextGrid.classList.add('grid__slider-button_disabled');
+  } else {
+    buttonNextGrid.classList.remove('grid__slider-button_disabled');
+  }
+}
+
+buttonPrevGrid.addEventListener('click', () => {
+  if (currentSlideGrid > 0) {
+    currentSlideGrid = setSlidePrev(currentSlideGrid);
+    showSlideGrid(currentSlideGrid);
+  }
+  updateButtons();
+  updatePagination();
+});
+
+buttonNextGrid.addEventListener('click', () => {
+  if (currentSlideGrid < slides.length - 1) {
+    currentSlideGrid = setSlideNext(currentSlideGrid);
+    showSlideGrid(currentSlideGrid);
+  }
+  updateButtons();
+  updatePagination();
+});
+
+// Добавляем обработчики для клика на точки пагинации
+paginationDots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    currentSlideGrid = index;
+    showSlideGrid(currentSlideGrid);
+    updateButtons();
+    updatePagination();
+  });
+});
+
+if (window.innerWidth <= 1024) {
+  showSlideGrid(currentSlideGrid);
+  updateButtons();
+  updatePagination();
+}
+
+const checkSliderGrid = () => {
+  if (window.innerWidth > 1024) {
+    const slider = document.querySelector('.grid__slider');
+    slider.innerHTML = '';
+    blocksForGrid.forEach(block => slider.appendChild(block));
+    blocksForGrid.forEach(block => block.style.display = 'flex');
+  } else {
+    showSlideGrid(currentSlideGrid);
+    updateButtons();
+    updatePagination();
+  }
+}
 
